7bbe96ea84882b9f60c84aa74df79b209ab13cd5
bbenskou <brahimbenskoura@gmail.com>
2024-08-20 18:00:08 +0100

    list search bar

--- func/home.go
+++ func/home.go
@@ -11,35 +11,61 @@ import (
 	d "gt/data"
 )
 
-var (
-	artists []d.Artist
-	artis   d.Locations
-)
+var artists []d.Artist
 
-func handleError(w http.ResponseWriter, status int, msg string,err error) {
+// artis   d.Locations
+
+func handleError(w http.ResponseWriter, status int, msg string, err error) {
 	http.Error(w, msg, status)
 	fmt.Println(err)
 }
+
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	query := strings.ToLower(r.URL.Query().Get("s"))
 	var results []d.SearchResult
 
-	for _, artist := range artists {
-		if len(results) > 5 {
+	for i, artist := range artists {
+		/*if len(results) > 10 {
 			break
+		}*/
+		if i == 0 {
+			for _, ar := range artists {
+				artistName2 := strings.ToLower(ar.Name)
+				if strings.HasPrefix(artistName2, query) {
+					results = append(results, d.SearchResult{
+						ID:   ar.ID,
+						Name: ar.Name,
+						Type: "artist/band",
+					})
+				}
+			}
 		}
-
 		artistName := strings.ToLower(artist.Name)
-		if strings.Contains(artistName, query) {
+		if !strings.HasPrefix(artistName, query) && strings.Contains(artistName, query) {
 			results = append(results, d.SearchResult{
 				ID:   artist.ID,
 				Name: artist.Name,
 				Type: "artist/band",
 			})
 		}
-
+	}
+	for i, artist := range artists {
+		if i == 0 {
+			for _, ar := range artists {
+				for _, member := range ar.Members {
+					artistName2 := strings.ToLower(member)
+					if strings.HasPrefix(artistName2, query) {
+						results = append(results, d.SearchResult{
+							ID:   ar.ID,
+							Name: member,
+							Type: "member of " + ar.Name,
+						})
+					}
+				}
+			}
+		}
 		for _, member := range artist.Members {
-			if strings.Contains(strings.ToLower(member), query) {
+			if !strings.HasPrefix(member, query) && strings.Contains(strings.ToLower(member), query) {
 				results = append(results, d.SearchResult{
 					ID:   artist.ID,
 					Name: member,
@@ -47,25 +73,50 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 				})
 			}
 		}
-
-		if strings.Contains(strings.ToLower(artist.FirstAlbum), query) {
+	}
+	for i, artist := range artists {
+		if i == 0 {
+			for _, ar := range artists {
+				artistName2 := strings.ToLower(ar.FirstAlbum)
+				if strings.HasPrefix(artistName2, query) {
+					results = append(results, d.SearchResult{
+						ID:   ar.ID,
+						Name: ar.FirstAlbum,
+						Type: "FirstAlbum of " + ar.Name,
+					})
+				}
+			}
+		}
+		if !strings.HasPrefix(strings.ToLower(artist.FirstAlbum), query) && strings.Contains(strings.ToLower(artist.FirstAlbum), query) {
 			results = append(results, d.SearchResult{
 				ID:   artist.ID,
 				Name: artist.FirstAlbum,
 				Type: "FirstAlbum of " + artist.Name,
 			})
 		}
-
+	
+		if i == 0 {
+			for _, ar := range artists {
+				C_Date := strconv.Itoa(ar.CreationDate)
+				if strings.HasPrefix(strings.ToLower(C_Date), query) {
+					results = append(results, d.SearchResult{
+						ID:   ar.ID,
+						Name: C_Date,
+						Type: "Creation Date of " + ar.Name,
+					})
+				}
+			}
+		}
 		C_Date := strconv.Itoa(artist.CreationDate)
-		if strings.Contains(strings.ToLower(C_Date), query) {
+		if !strings.HasPrefix(strings.ToLower(C_Date), query) && strings.Contains(strings.ToLower(C_Date), query) {
 			results = append(results, d.SearchResult{
 				ID:   artist.ID,
 				Name: C_Date,
 				Type: "Creation Date of " + artist.Name,
 			})
 		}
-
-		if err := fetchAndDecode(artist.Locations, &artis); err != nil {
+	}
+		/*if err := fetchAndDecode(artist.Locations, &artis); err != nil {
 			handleError(w, http.StatusInternalServerError, "Internal Server Error 500",err)
 			return
 		}
@@ -78,38 +129,37 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 					Type: "location of " + artist.Name,
 				})
 			}
-		}
-	}
+		}*/
+	
 
-	if len(results) > 5 {
-		results = results[:5]
-	}
+	/*if len(results) > 10 {
+		results = results[:10]
+	}*/
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(results); err != nil {
-		handleError(w, http.StatusInternalServerError, "Internal Server Error 500",err)
+		handleError(w, http.StatusInternalServerError, "Internal Server Error 500", err)
 	}
 }
 
-
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		handleError(w, http.StatusNotFound, "page not found 404",nil)
+		handleError(w, http.StatusNotFound, "page not found 404", nil)
 		return
 	}
 
 	if err := fetchAndDecode("https://groupietrackers.herokuapp.com/api/artists", &artists); err != nil {
-		handleError(w, http.StatusInternalServerError, "Internal Server Error 500",err)
+		handleError(w, http.StatusInternalServerError, "Internal Server Error 500", err)
 		return
 	}
 
 	tmp, err := template.ParseFiles("template/home_page.html")
 	if err != nil {
-		handleError(w, http.StatusInternalServerError, "Internal Server Error 500",err)
+		handleError(w, http.StatusInternalServerError, "Internal Server Error 500", err)
 		return
 	}
 
 	if err := tmp.Execute(w, artists); err != nil {
-		handleError(w, http.StatusInternalServerError, "Internal Server Error 500",err)
+		handleError(w, http.StatusInternalServerError, "Internal Server Error 500", err)
 	}
 }
